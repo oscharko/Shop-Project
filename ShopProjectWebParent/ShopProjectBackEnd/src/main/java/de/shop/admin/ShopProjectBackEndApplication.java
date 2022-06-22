92ac50642dcde140b8338307e4e34c35ea77c0c5
oscharko <o.scharkowski@oscharko.de>
2022-06-22 16:03:17 +0200

    Initial commit

---
+++ ShopProjectWebParent/ShopProjectBackEnd/src/main/java/de/shop/admin/ShopProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package de.shop.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ ShopProjectWebParent/ShopProjectFrontEnd/src/main/java/de/shop/ShopProjectFrontEndApplication.java
@@ -0,0 +1,13 @@
+package de.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopProjectFrontEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopProjectFrontEndApplication.class, args);
+	}
+
+}
